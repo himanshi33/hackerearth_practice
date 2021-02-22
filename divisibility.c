cc1da347fd44b3cf1fc64eaf982c0ff9e6f5e959
Hiimaanshi dogra <60709487+himanshi33@users.noreply.github.com>
2021-02-22 17:51:16 +0530

    Create divisibility.c

---
+++ divisibility.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	long int n;
+	long int a;
+	scanf("%ld",&n);
+	for(long i=1;i<=n;i++){
+	scanf("%ld ",&a);}
+	if(a%10==0)
+	printf("Yes");
+	else
+	printf("No");
+	}
+
