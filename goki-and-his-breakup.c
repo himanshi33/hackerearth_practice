44c0123ce006a58b8091690271979093f2099739
Hiimaanshi dogra <60709487+himanshi33@users.noreply.github.com>
2021-02-22 17:48:50 +0530

    Create goki-and-his-breakup.c

---
+++ goki-and-his-breakup.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,x,Q;
+	scanf("%d",&n);
+	scanf("%d",&x);
+	for( int i=1;i<=n;i++){
+		scanf("%d",&Q);
+	if(Q>=x)
+	printf("YES\n");
+	else
+	printf("NO\n");
+
+}
+}
